ca5ccd6898813b81da245e4fb3b8da2decbbc358
Nicolas Velez <nicolas.velez99@hotmail.com>
2022-12-30 16:28:09 -0500

    [feat]: se crea controlador y modelo para el documento

---
+++ src/app/controllers/DocumentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Acris\App\Controllers;
+
+class DocumentController 
+{
+    public function manageDocument($vars){
+        // var_dump($vars);
+        // echo 'manejo el documento';
+    }
+}
---
+++ src/app/models/Document.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Acris\App\Models;
+
+class Document
+{
+    
+}
