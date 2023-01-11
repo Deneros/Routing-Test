99380f657ef3275181c77d186b714a69aca29b1e
Nicolas Velez <nicolas.velez99@hotmail.com>
2023-01-11 09:28:26 -0500

    [feat]:Se agrega nuevos metodos para controlar la sesion

---
+++ src/app/libs/Session.php
@@ -0,0 +1,26 @@
+<?php namespace Acris\App\Libs;
+
+class Session
+{
+    private bool $flash = false;
+
+    public function __construct()
+    {   
+        session_start();
+    }
+
+    public function get($key){
+        return $_SESSION[$key];
+    }
+
+    public function set($key, $data){
+        $_SESSION[$key]=$data;
+    }
+
+    public function flash($data){
+        $this->flash=true;
+        $this->set('flash', $data);
+    }
+
+
+}
