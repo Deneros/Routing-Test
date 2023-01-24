0567103c456552024aab77d3ca0481bb85dd3bcc
Nicolas Velez <nicolas.velez99@hotmail.com>
2023-01-24 12:10:06 -0500

    [build]: se cambia la ubicacion de los archivos publicos para que sean el unico punto de acceso

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-require_once './src/Routes/web.php';
-
-
-
-
-
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../vendor/autoload.php';
+require_once '../src/routes/web.php';
+
+// require_once './vendor/autoload.php';
+// require_once './src/Routes/web.php';
+
+// use Acris\App\Libs\Session;
+// $session = new Session;
+
+
+
+
+
+
+
